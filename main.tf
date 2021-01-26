edf510fba0bcd78bfc66be774a1b3d9b1e4b561e
Ed Lipski <elipski@gmail.com>
2021-01-26 11:12:12 -0500

    Updated network security group deny-all subnet with VirtualNetwork and added terraform apply output screenshot per Udacity reviewer feedback

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_network_security_group" "main" {
     source_port_range          = "*"
     destination_port_range     = "*"
     source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    destination_address_prefix = "VirtualNetwork"
   }
 }
 
